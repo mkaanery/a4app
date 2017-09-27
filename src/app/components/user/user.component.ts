6f74f2176e94d4ef5b16b87022f28a0214c7c1fe
mkaanery <mahmutkaan.eryilmaz@agu.edu.tr>
2017-09-27 15:14:24 +0300

    end.

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  name:string;
+  age:number;
+  email:string;
+  address:Address;
+  hobbies:string[];
+  hello:any;
+  posts:Post[];
+  isEdit:boolean = false;
+
+  constructor(private dataService:DataService) {
+    console.log('constructor ran...');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit ran...');
+    this.name = "John Doe";
+    this.age = 30;
+    this.email = "a@b";
+    this.address = {
+      street: '50 Main st',
+      city: 'Boston',
+      state: 'MA'
+    }
+    this.hobbies = ['write code', 'sleeping'];
+    this.hello = true;
+    this.dataService.getPosts().subscribe(posts => {
+      // console.log(posts);
+      this.posts = posts;
+    });
+  }
+
+  onClick() {
+    this.name = 'Brad Traversy';
+    this.hobbies.push('New hobby');
+  }
+
+  addHobby(hobby) {
+    console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+
+  deleteHobby(hobby) {
+    for(let i = 0; i < this.hobbies.length; i++) {
+      if(this.hobbies[i] == hobby) {
+        this.hobbies.splice(i, 1);
+      }
+     }
+  }
+
+  toggleEdit() {
+    this.isEdit = !this.isEdit;
+  }
+
+}
+
+interface Address {
+  street:string,
+  city:string,
+  state:string
+}
+
+interface Post {
+  id:number,
+  title:string,
+  body:string,
+  userId:number
+}
